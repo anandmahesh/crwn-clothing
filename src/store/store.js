299c1900e8ea0fff4661d37a6c6996bd9a0dc7d1
manand <mahesh.anand@soprasteria.com>
2025-06-21 13:28:16 +0530

    redux-persist implmeneted for local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 //import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist'
 
 const loggMiddleWare = (store) => (next) => (action) => {
     if (!action.type) {
@@ -17,9 +19,20 @@ const loggMiddleWare = (store) => (next) => (action) => {
 
 }
 
+const presistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
 //const middleWares = [logger];
 const middleWares = [loggMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(presistConfig, rootReducer);
+
+//export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
